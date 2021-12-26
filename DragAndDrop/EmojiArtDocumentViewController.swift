150ed47f8a872f6a2b23d0856f3e37f26e3bc132
NickSagan <9952728@gmail.com>
2021-12-26 18:09:36 +0300

    add emojiCollectionViewCell

--- DragAndDrop/EmojiArtDocumentViewController.swift
+++ DragAndDrop/EmojiArtDocumentViewController.swift
@@ -9,14 +9,18 @@ import UIKit
 
 class EmojiArtDocumentViewController: UITableViewController {
     
-    var emojiArtDocuments = ["One", "Two", "Three"]
+    // MARK: - Model
 
+    var emojiArtDocuments = ["One", "Two", "Three"]
+    
+    // MARK: - Target/Action
+    
     @IBAction func newEmojiArt(_ sender: UIBarButtonItem) {
         emojiArtDocuments += ["Untitled".madeUnique(withRespectTo: emojiArtDocuments)]
         tableView.reloadData()
     }
     
-    // MARK: - Table view data source
+    // MARK: - UITableViewDataSource
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         return 1
@@ -31,9 +35,10 @@ class EmojiArtDocumentViewController: UITableViewController {
 
         // Configure the cell...
         cell.textLabel?.text = emojiArtDocuments[indexPath.row]
+
         return cell
     }
-
+    
     /*
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
@@ -42,8 +47,6 @@ class EmojiArtDocumentViewController: UITableViewController {
     }
     */
 
-
-    // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
             // Delete the row from the data source
@@ -51,7 +54,7 @@ class EmojiArtDocumentViewController: UITableViewController {
             tableView.deleteRows(at: [indexPath], with: .fade)
         } else if editingStyle == .insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+        }
     }
 
     /*
@@ -69,11 +72,22 @@ class EmojiArtDocumentViewController: UITableViewController {
     }
     */
 
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    // MARK: - View Controller Lifecycle
+    
     override func viewWillLayoutSubviews() {
         super.viewWillLayoutSubviews()
         if splitViewController?.preferredDisplayMode != .primaryOverlay {
             splitViewController?.preferredDisplayMode = .primaryOverlay
         }
     }
-
 }
--- DragAndDrop/EmojiArtView.swift
+++ DragAndDrop/EmojiArtView.swift
@@ -7,16 +7,161 @@
 
 import UIKit
 
-class EmojiArtView: UIView {
+class EmojiArtView: UIView, UIDropInteractionDelegate {
+    // MARK: - Initialization
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+    }
     
-    var backgroundImage: UIImage? {
-        didSet {
-            setNeedsDisplay()
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setup()
+    }
+    
+    private func setup() {
+        addInteraction(UIDropInteraction(delegate: self))
+    }
+    
+    // this var is not in Demo
+    private var font: UIFont {
+        return
+            UIFontMetrics(forTextStyle: .body).scaledFont(for: UIFont.preferredFont(forTextStyle: .body).withSize(64.0))
+    }
+    
+    // MARK: - UIDropInteractionDelegate
+    
+    func dropInteraction(_ interaction: UIDropInteraction, canHandle session: UIDropSession) -> Bool {
+        return session.canLoadObjects(ofClass: NSAttributedString.self)
+    }
+    
+    func dropInteraction(_ interaction: UIDropInteraction, sessionDidUpdate session: UIDropSession) -> UIDropProposal {
+        return UIDropProposal(operation: .copy)
+    }
+    
+    func dropInteraction(_ interaction: UIDropInteraction, performDrop session: UIDropSession) {
+        session.loadObjects(ofClass: NSAttributedString.self) { providers in
+            let dropPoint = session.location(in: self)
+            for attributedString in providers as? [NSAttributedString] ?? [] {
+                self.addLabel(with: attributedString, centeredAt: dropPoint)
+            }
         }
     }
+    
+    private func addLabel(with attributedString: NSAttributedString, centeredAt point: CGPoint) {
+        let label = UILabel()
+        label.backgroundColor = .clear
+        // next row is not in Demo
+        label.attributedText = attributedString.font != nil ? attributedString : NSAttributedString(string: attributedString.string,attributes: [.font:self.font])
+    //   label.attributedText = attributedString
+        label.sizeToFit()
+        label.center = point
+        addEmojiArtGestureRecognizers(to: label)
+        addSubview(label)
+    }
+    
+    // MARK: - Drawing the Background
+    
+    var backgroundImage: UIImage? { didSet { setNeedsDisplay() } }
     
     override func draw(_ rect: CGRect) {
         backgroundImage?.draw(in: bounds)
     }
+}
+
+// Gesture Recognition Extension to EmojiArtView
+
+extension EmojiArtView
+{
+    func addEmojiArtGestureRecognizers(to view: UIView) {
+        view.isUserInteractionEnabled = true
+        view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(self.selectSubview(by:))))
+        view.addGestureRecognizer(UIPanGestureRecognizer(target: self, action: #selector(self.selectAndMoveSubview(by:))))
+    }
+
+    var selectedSubview: UIView? {
+        get { return subviews.filter { $0.layer.borderWidth > 0 }.first }
+        set {
+            subviews.forEach { $0.layer.borderWidth = 0 }
+            newValue?.layer.borderWidth = 1
+            if newValue != nil {
+                enableRecognizers()
+            } else {
+                disableRecognizers()
+            }
+        }
+    }
+
+    @objc func selectSubview(by recognizer: UITapGestureRecognizer) {
+        if recognizer.state == .ended {
+            selectedSubview = recognizer.view
+        }
+    }
     
+    @objc func selectAndMoveSubview(by recognizer: UIPanGestureRecognizer) {
+        switch recognizer.state {
+        case .began:
+            if selectedSubview != nil, recognizer.view != nil {
+                selectedSubview = recognizer.view
+            }
+        case .changed, .ended:
+            if selectedSubview != nil {
+                recognizer.view?.center = recognizer.view!.center.offset(by: recognizer.translation(in: self))
+                recognizer.setTranslation(CGPoint.zero, in: self)
+            }
+        default:
+            break
+        }
+    }
+    
+    func enableRecognizers() {
+        if let scrollView = superview as? UIScrollView {
+            // if we are in a scroll view, disable its recognizers
+            // so that ours will get the touch events instead
+            scrollView.panGestureRecognizer.isEnabled = false
+            scrollView.pinchGestureRecognizer?.isEnabled = false
+        }
+        if gestureRecognizers == nil || gestureRecognizers!.count == 0 {
+            addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(self.deselectSubview)))
+            addGestureRecognizer(UIPinchGestureRecognizer(target: self, action: #selector(self.resizeSelectedLabel(by:))))
+        } else {
+            gestureRecognizers?.forEach { $0.isEnabled = true }
+        }
+    }
+    
+    func disableRecognizers() {
+        if let scrollView = superview as? UIScrollView {
+            // if we are in a scroll view, re-enable its recognizers
+            scrollView.panGestureRecognizer.isEnabled = true
+            scrollView.pinchGestureRecognizer?.isEnabled = true
+        }
+        gestureRecognizers?.forEach { $0.isEnabled = false }
+    }
+    
+    @objc func deselectSubview() {
+        selectedSubview = nil
+    }
+    
+    @objc func resizeSelectedLabel(by recognizer: UIPinchGestureRecognizer) {
+        switch recognizer.state {
+        case .changed, .ended:
+            if let label = selectedSubview as? UILabel {
+                label.attributedText = label.attributedText?.withFontScaled(by: recognizer.scale)
+                label.stretchToFit()
+                recognizer.scale = 1.0
+            }
+        default:
+            break
+        }
+    }
+    
+    @objc func selectAndSendSubviewToBack(by recognizer: UITapGestureRecognizer) {
+        if recognizer.state == .ended {
+            if let view = recognizer.view, let index = subviews.index(of: view) {
+                selectedSubview = view
+                exchangeSubview(at: 0, withSubviewAt: index)
+            }
+        }
+    }
 }
